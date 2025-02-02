ccbf6ed75c8c456d1fa15fd4f0c9ce8a44f652ab
Bob-Zimmerman <63750975+Bob-Zimmerman@users.noreply.github.com>
2025-02-02 16:41:27 -0600

    I forgot to make data types public and constants non-isolated

--- Sources/CheckPointManagementConnection/CPM Data Types.swift
+++ Sources/CheckPointManagementConnection/CPM Data Types.swift
@@ -9,31 +9,31 @@ import Foundation
 
 public let CPMAPIErrorDomain: String = "CPMAPIErrorDomain"
 
-enum CPMError: Error {
-	static let unknownError = NSError(domain: CPMAPIErrorDomain, code: -1, userInfo: [
+public enum CPMError: Error {
+	public static let unknownError = NSError(domain: CPMAPIErrorDomain, code: -1, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Unknown Error", comment: "A connection error indicating something has gone wrong, but I don't yet handle it."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("Something has gone wrong in a way I have never seen before and don't currently handle.", comment: "Informative text describing a connection error.") ]) // swiftlint:disable:this line_length
-	static let apiDown = NSError(domain: CPMAPIErrorDomain, code: -2, userInfo: [
+	public static let apiDown = NSError(domain: CPMAPIErrorDomain, code: -2, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("API Appears to be Down", comment: "A connection error indicating the server responded to our request, but that the response indicated the service has failed."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("It looks like we can reach the server, but the API service is down. Please check the API status.", comment: "Informative text describing a connection error.") ]) // swiftlint:disable:this line_length
-	static let badCredentials = NSError(domain: CPMAPIErrorDomain, code: -400, userInfo: [
+	public static let badCredentials = NSError(domain: CPMAPIErrorDomain, code: -400, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Access Denied", comment: "A connection error indicating the user could not authenticate."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("It looks like the server refused to accept the credentials you provided. Please try entering your credentials again.", comment: "Informative text describing a connection error.") ]) // swiftlint:disable:this line_length
-	static let connectionProhibited = NSError(domain: CPMAPIErrorDomain, code: -403, userInfo: [
+	public static let connectionProhibited = NSError(domain: CPMAPIErrorDomain, code: -403, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Permission Denied", comment: "A connection error indicating the user was able to authenticate, but does not have permission to use the API."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("It looks like the server accepted your credentials, but your user does not have permission to use the API. Please confirm your permissions on the server.", comment: "Informative text describing a connection error.") ]) // swiftlint:disable:this line_length
-	static let tooManyRequests = NSError(domain: CPMAPIErrorDomain, code: -3, userInfo: [
+	public static let tooManyRequests = NSError(domain: CPMAPIErrorDomain, code: -3, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Too Many Requests", comment: "A connection error indicating the user has tried to log in too many times in quick succession."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("You have hit the server's rate limit. Please wait a few minutes or use the command 'api throttling off' on the server to disable the rate limiting.", comment: "Informative text describing a connection error.") ]) // swiftlint:disable:this line_length
-	static let writeCallInReadOnlySession = NSError(domain: CPMAPIErrorDomain, code: -4, userInfo: [
+	public static let writeCallInReadOnlySession = NSError(domain: CPMAPIErrorDomain, code: -4, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Write Call in a Read-Only Session", comment: "An error indicating the user has tried to write something, but they're using a read-only session."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("Your session is read-only, but you made a call to write something. Try again after connecting in write mode.", comment: "Informative text describing a session error.") ]) // swiftlint:disable:this line_length
-	static let invalidObject = NSError(domain: CPMAPIErrorDomain, code: -5, userInfo: [
+	public static let invalidObject = NSError(domain: CPMAPIErrorDomain, code: -5, userInfo: [
 		NSLocalizedDescriptionKey: NSLocalizedString("Call made to change an invalid object", comment: "An error indicating the user has tried to perform an action, but the object they tried to perform it on doesn't exist."), // swiftlint:disable:this line_length
 		NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString("You made a call to perform an action with an object, but the object doesn't exist.", comment: "Informative text describing a session error.") ]) // swiftlint:disable:this line_length
 }
 
-struct CPMDate: Codable {
+public struct CPMDate: Codable, Sendable {
 	let iso8601: String
 	let posix: Date
 	
@@ -43,7 +43,7 @@ struct CPMDate: Codable {
 	}
 }
 
-struct CPMLoginResponse: Codable, Identifiable {
+internal struct CPMLoginResponse: Codable, Identifiable, Sendable {
 	let apiServerVersion: String
 	let id: UUID?
 	let lastLogin: Date?
@@ -78,11 +78,27 @@ struct CPMLoginResponse: Codable, Identifiable {
 	}
 }
 
-struct PolicyPushParameters {
+public struct PolicyPushParameters: Sendable {
 	let access: Bool
 	let desktopSecurity: Bool
 	let qos: Bool
 	let threatPrevention: Bool
 	let installOnAllClusterMembersOrFail: Bool
 	let prepareOnly: Bool
+	
+	public init(
+		access: Bool,
+		desktopSecurity: Bool,
+		qos: Bool,
+		threatPrevention: Bool,
+		installOnAllClusterMembersOrFail: Bool,
+		prepareOnly: Bool)
+	{
+		self.access = access
+		self.desktopSecurity = desktopSecurity
+		self.qos = qos
+		self.threatPrevention = threatPrevention
+		self.installOnAllClusterMembersOrFail = installOnAllClusterMembersOrFail
+		self.prepareOnly = prepareOnly
+	}
 }
